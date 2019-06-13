4240f07c80af521d016ccdedf29e07e2a225d2ee
t-kojima <t-kojima@sonicgarden.jp>
2019-06-13 19:45:00 +0900

    feat: mutation observer

--- src/js/background.js
+++ src/js/background.js
@@ -1,3 +1,6 @@
+import '../img/icon-128.png'
+import '../img/icon-32.png'
+
 let hidden = true
 
 chrome.browserAction.onClicked.addListener(function(tab) {
--- src/js/index.js
+++ src/js/index.js
@@ -6,11 +6,17 @@ const fields = [
     .reduce((x, y) => [...x, ...Object.values(y)], [])
 ]
 
-fields.forEach(({ id, type, var: fieldCode }) => {
-  const [element] = document.getElementsByClassName(`field-${id}`)
-  element &&
-    element.insertAdjacentHTML(
-      'beforeend',
-      `<div class="mieru notification is-primary" style="display: none"><b class="field-code">${fieldCode}</b><small>(${type})</small></div>`
-    )
-})
+const addMieruElements = () => {
+  fields.forEach(({ id, type, var: fieldCode }) => {
+    const [element] = document.getElementsByClassName(`field-${id}`)
+    element &&
+      element.insertAdjacentHTML(
+        'beforeend',
+        `<div class="mieru notification is-primary" style="display: none"><b class="field-code">${fieldCode}</b><small>(${type})</small></div>`
+      )
+  })
+}
+
+new MutationObserver(() => { addMieruElements() }).observe(document.getElementById('record-gaia'), {childList: true});
+
+addMieruElements()
