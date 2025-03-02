bc64ebf77b848c179530e3014e91d6ab4793ce7e
t-kojima <t-kojima@sonicgarden.jp>
2025-03-02 10:26:48 +0900

    feat: アプリ一覧を広げるように

--- src/background.ts
+++ src/background.ts
@@ -2,9 +2,7 @@ import './img/icon-128.png';
 import './img/icon-32.png';
 
 let hidden = true;
-console.log('load background');
 chrome.action.onClicked.addListener(function (tab) {
-  console.log('clicked', tab);
   if (tab.id) {
     chrome.tabs.sendMessage(tab.id, hidden);
     hidden = !hidden;
--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-import "./css/style.css";
+import './css/style.css';
 
 const injectScript = (filePath: string, tag: string) => {
   var node = document.getElementsByTagName(tag)[0];
@@ -10,9 +10,16 @@ const injectScript = (filePath: string, tag: string) => {
 injectScript(chrome.runtime.getURL('src/index.js'), 'body');
 
 chrome.runtime.onMessage.addListener(function (request) {
-  // console.log('recieve message', request);
-  const elements = [...document.querySelectorAll('.mieru')] as HTMLDivElement[];
-  Array.from(elements).forEach((element) => {
-    element.style.display = request ? 'block' : 'none';
-  });
+  {
+    const elements = [...document.querySelectorAll('.mieru')] as HTMLDivElement[];
+    Array.from(elements).forEach((element) => {
+      element.style.display = request ? 'block' : 'none';
+    });
+  }
+  {
+    const element = document.querySelector('.gaia-argoui-space-spacecontent-right') as HTMLDivElement;
+    if (element) {
+      element.style.width = request ? '600px' : '';
+    }
+  }
 });
